edec8db47a0fcfdb08f07f85f0ebbb391deb6ca9
Davide Baldi <d.baldi88@gmail.com>
2025-04-05 11:34:13 +0200

    tailwind and nuxt image installed and tested

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,18 @@
 <template>
-    <h1>homepage</h1>
+    <main>
+        <h1>homepage</h1>
+        <NuxtImg  src="/davide.png" class="rounded-full w-32"/>
+        <p>Hello! It ´s a me, Davide </p>
+        <p>software engineer based in Stockholm</p>
+
+        <p class="bg-slate-400">I love to find solutions to complex it problem!</p>
+
+        <p>My area expertise: are frontend </p>
+        <p>web development, frontend enginner and optimization, cloud integrations and strong knowledge of SQL and data analytics.</p>
+        <div></div>
+
+
+
+    </main>
+    
 </template>
