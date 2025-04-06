8ce2b6817a297538307ddc668a8d1b02fc346841
Davide Baldi <d.baldi88@gmail.com>
2025-04-06 19:26:06 +0200

    add project in page and site under construciton where necessary

--- pages/contact/index.vue
+++ pages/contact/index.vue
@@ -1,3 +1,4 @@
 <template>
     <h1>contact</h1>
+    <h1 class="text-6xl text-black">PAGE UNDER COSTRUCTION</h1>
 </template>
--- pages/projects/index.vue
+++ pages/projects/index.vue
@@ -1,4 +1,3 @@
 <template>
-    <h1>projects</h1> 
-
+    <ProjectsDisplay />
 </template>
