33675706a0b4b00473bf9e2a8bd74d23987721ee
Davide Baldi <62833639+indiano881@users.noreply.github.com>
2025-04-25 11:42:51 +0200

    added gcp data practitioner badge

--- utils/certification.js
+++ utils/certification.js
@@ -1,5 +1,11 @@
 export const certifications = [
   {
+    title: 'GCP Associate Data Practitioner',
+    image: 'acedata.png',
+    number: '39e37beef025441f8bbb6bbc40821a06',
+    expiration: '2028-04-23',
+  },
+  {
     title: 'GCP Cloud Digital Leader',
     image: 'gcp.png',
     number: '9f3177294b964a7d8d6cbbf61ee2e86b',
